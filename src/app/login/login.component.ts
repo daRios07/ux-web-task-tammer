05601bf82d941113fc00ef5a3577927968eb97bf
Inés Rojas <ia.rojas2@uniandes.edu.co>
2024-03-11 23:11:30 +0100

    add login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports:[RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+//  constructor(private router: Router) { }
+
+  iniciarSesion = function () {
+  //  this.router.navigateByUrl('/user');
+  };
+  routerLink="/statistics"
+}
