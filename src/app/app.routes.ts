85fb555fd6d9ab18fa0d51cfcc666b32532e73f7
Inés Rojas <ia.rojas2@uniandes.edu.co>
2024-03-12 00:12:15 +0100

    statistics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {StatisticsComponent} from './statistics/statistics.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'statistics', component: StatisticsComponent },
 ];
---
+++ src/app/statistics/statistics.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-statistics',
+  standalone: true,
+  imports:[RouterModule],
+  templateUrl: './statistics.component.html',
+  styleUrl: './statistics.component.css'
+})
+export class StatisticsComponent {
+
+}
