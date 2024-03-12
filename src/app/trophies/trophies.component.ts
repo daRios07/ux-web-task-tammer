8893367a8514ca58703c8d3f57995df7e0514553
dario <jd.rios2@uniandes.edu.co>
2024-03-11 19:26:29 -0500

    trophies

---
+++ src/app/trophies/trophies.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-trophies',
+  standalone: true,
+  imports:[RouterModule],
+  templateUrl: './trophies.component.html',
+  styleUrl: './trophies.component.css'
+})
+export class TrophiesComponent {
+
+}
