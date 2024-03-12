30497bb29eba90cabec1464b8f2d15b1355b4961
dario <jd.rios2@uniandes.edu.co>
2024-03-11 19:29:24 -0500

    trophies route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {StatisticsComponent} from './statistics/statistics.component';
+import {TrophiesComponent} from './trophies/trophies.component';
+
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'statistics', component: StatisticsComponent },
+    { path: 'trophies', component: TrophiesComponent },
 ];
